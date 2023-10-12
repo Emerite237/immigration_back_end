b2b4576e38b6a0eb375651a617acc774fc01e17c
emerites45 <emerites45@gmail.com>
2023-10-12 14:05:03 +0100

    ajout des routes pour la connexion

--- src/routes/connexion.js
+++ src/routes/connexion.js
@@ -9,12 +9,18 @@ module.exports= (app) => {
     
     User.findOne({ where: {pseudo: req.body.pseudo} }).then( user =>{
 
-            return res.json({'token:':Token.generetedTokenForUser(user)})
+        if(user === null){
+            const message = `speudo incorrect`
+            return res.status(404).json({message})
+          }
+
+
+            return res.json({'token:':Token.generetedTokenForUser(user),user})
         }
     ).catch(error =>{
         const message =" l' utilisateur n'a pas pue se connecte , reesayer dans quelque instants..."
         console.log(error)
-        return res.json({message, data:error})
+        return res.status(400).json({message, data:error})
     } )
 
     } )
